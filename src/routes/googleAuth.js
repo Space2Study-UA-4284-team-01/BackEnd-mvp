2116a8d5a564d2ff089c5f531eb381734c725acd
Yevheniia-B <evgenijar.work@gmail.com>
2026-03-13 16:48:28 +0200

    feat: implement google auth

---
+++ src/routes/googleAuth.js
@@ -0,0 +1,7 @@
+const router = require('express').Router()
+const googleAuthController = require('~/controllers/googleAuth')
+const  asyncWrapper  = require('~/middlewares/asyncWrapper') 
+
+router.post('/', asyncWrapper(googleAuthController.googleLogin))
+
+module.exports = router
