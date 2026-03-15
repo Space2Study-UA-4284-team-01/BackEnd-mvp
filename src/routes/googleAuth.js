8427b5eaec6a2b390c27d0b954b81bef3e64bc39
Yevheniia-B <evgenijar.work@gmail.com>
2026-03-15 23:52:06 +0200

    fix: Fix Google Auth logic and test isolation

--- src/routes/googleAuth.js
+++ src/routes/googleAuth.js
@@ -1,7 +1,8 @@
 const router = require('express').Router()
 const googleAuthController = require('~/controllers/googleAuth')
-const  asyncWrapper  = require('~/middlewares/asyncWrapper') 
+const asyncWrapper = require('~/middlewares/asyncWrapper') 
 
 router.post('/', asyncWrapper(googleAuthController.googleLogin))
+
 
 module.exports = router
