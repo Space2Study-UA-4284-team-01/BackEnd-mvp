08d8609934fc79336866db97969f505e05d8743c
llevchenkosd@gmail.com <Glist007>
2026-03-22 18:47:03 +0200

    fix returned data type

--- src/services/question.js
+++ src/services/question.js
@@ -17,7 +17,7 @@ const questionService = {
   },
 
   getQuestionById: async (id) => {
-    return await Question.findById(id).lean().exec()
+    return await Question.findById(id).populate({ path: 'category', select: '_id name' }).lean().exec()
   },
 
   createQuestion: async (author, data) => {
